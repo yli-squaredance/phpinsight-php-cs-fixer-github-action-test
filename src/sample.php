4e81ad7a93329a3ab665bf694152beb59d57f33f
yli-squaredance <yixin.li@squaredance.io>
2023-01-16 17:11:29 -0500

    add php insight config file and gh action .yml file

---
+++ src/sample.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+$foo = 'foo';
+$bar = 'bar';
+$superLongName = 'super long name';
+
+$dummyArray = [
+    'one' => 'sample 1',
+    'two' => 'sample 2',
+    'three' => 'sample 3',
+];
