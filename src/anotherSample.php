015e43a359fa5e38f2179b248f0542cec1bccbbf
yli-squaredance <yli-squaredance@users.noreply.github.com>
2023-01-17 20:51:01 +0000

    Fix styling changes

--- src/anotherSample.php
+++ src/anotherSample.php
@@ -1,7 +1,9 @@
 <?php
 
-$test1 = 'this is a test';
-$longTest = 'this is a long test';
+declare(strict_types=1);
+
+$test1                     = 'this is a test';
+$longTest                  = 'this is a long test';
 $superLooooooooooooongTest = 'this is a super looooooong test';
 
 $company = new Company($longTest);
