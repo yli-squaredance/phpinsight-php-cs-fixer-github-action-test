2593337e131b443aa5f5875902789319882d73c9
yli-squaredance <yixin.li@squaredance.io>
2023-01-17 15:23:49 -0500

    recover sample.php to unformatted file

--- src/sample.php
+++ src/sample.php
@@ -2,13 +2,13 @@
 
 declare(strict_types=1);
 
-$foo           = 'foo';
-$bar           = 'bar';
+$foo = 'foo';
+$bar = 'bar';
 $superLongName = 'super long name';
 
 $dummyArray = [
-    'one'   => 'sample 1',
-    'two'   => 'sample 2',
+    'one' => 'sample 1',
+    'two' => 'sample 2',
     'three' => 'sample 3',
 ];
 
